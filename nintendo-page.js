b32e604e473dac3a9b75a10c032757784962e3fc
Irene <grim6ace@gmail.com>
2025-11-17 23:50:43 +0300

    ver 10: dropdown

--- nintendo-page.js
+++ nintendo-page.js
@@ -1,31 +1,37 @@
+/* HEADER DROPDOWN MENU */
 const headerButtons = document.querySelectorAll('.header-buttons button');
 const overlay = document.querySelector('.modal-overlay');
-const dropContent = document.querySelector('.dropdown-content');
-const dropdown = document.querySelector('.explore-dropdown'); 
-console.log(dropContent.scrollHeight);
+const dropContentContainer = document.querySelector('.dropdown-content');
+const dropdownContents = document.querySelectorAll('.dropdown'); 
 
 
 headerButtons.forEach(btn => {
   btn.addEventListener('click', () => {
-    dropdown.style.display = "block";
-    const fullHeight = dropContent.scrollHeight;
-    dropContent.classList.add('open-drop');
-    dropContent.style.maxHeight = fullHeight + 'px';
+    const menuOption = btn.dataset.option;
+    const dropdownContent = document.getElementById(menuOption);
 
+    dropdownContents.forEach(content => content.style.display = "none");
+    
+    dropdownContent.style.display = "block";
+    dropContentContainer.classList.remove('open-drop');
+  /*   dropContentContainer.style.maxHeight = '0px'; */
+
+    /* const fullHeight = dropContentContainer.scrollHeight; */
+    
+    dropContentContainer.classList.add('open-drop');
+    dropContentContainer.style.maxHeight = dropContentContainer.scrollHeight + 'px';
+  /*   dropContentContainer.style.maxHeight = fullHeight + 'px'; */
 
     overlay.classList.add('open-modal');
-
   })
 });
 
 function closeModal() {
- /*  const startHeight = dropContent.scrollHeight;
-  dropContent.style.maxHeight = startHeight + 'px'; */
+  dropContentContainer.classList.remove('open-drop');
+  dropContentContainer.style.maxHeight = '0px';
+  dropdownContents.forEach(content => content.style.display = "none");
 
-  dropContent.style.maxHeight = '0px';
-  dropContent.classList.remove('open-drop');
   overlay.classList.remove('open-modal');
-
 };
 
 overlay.addEventListener('click', (e) => {
